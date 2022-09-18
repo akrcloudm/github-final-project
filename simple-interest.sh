d114ee5fde9052083ff2603071612f86ee94d720
akrcloudm <112798071+akrcloudm@users.noreply.github.com>
2022-09-18 11:43:44 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: AKR
    # Additional Authors:
-   # <your GitHub username>
+   # <akrcloudm>
 
    # Input:
    # p, principal amount
